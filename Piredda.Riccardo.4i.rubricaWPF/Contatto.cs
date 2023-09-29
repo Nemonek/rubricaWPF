ebc03399ef60ecc3a74d9e55d8819cdb2e33853f
Riccardo Piredda <127589992+Nemonek@users.noreply.github.com>
2023-09-29 17:46:35 +0200

    Aggiungere i file di progetto.

---
+++ Piredda.Riccardo.4i.rubricaWPF/Contatto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piredda.Riccardo._4i.rubricaWPF
+{
+    internal class Contatto
+    {
+        // The microsoft convention for the private attributes of a class is:
+        // for privates member use the underscore before the name;
+        // for the property use the first letter lower case.
+        private string _nome;
+        private string _cognome;
+        private string _numero;
+
+        //public Contatto(string nom, string cog, string num)
+        //{
+        //    Nome = nom;
+        //    Cognome = cog;
+        //    Numero = num;
+        //}
+
+        public string Nome { get => _nome; set => _nome = value; }
+        public string Cognome { get => _cognome; set => _cognome = value; }
+        public string Numero { get => _numero; set => _numero = value; }
+    }
+}
---
+++ Piredda.Riccardo.4i.rubricaWPF/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Piredda.Riccardo._4i.rubricaWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Contatto c = new();
+            c.Nome = string.Empty;
+            c.Cognome = string.Empty;
+            c.Numero = string.Empty;
+
+            Contatto[] Contatti = new Contatto[100];
+            Contatti[0] = c;
+
+            Contatti[0].Nome = "Riccardo";
+            Contatti[0].Cognome = "Piredda";
+        }
+    }
+}
