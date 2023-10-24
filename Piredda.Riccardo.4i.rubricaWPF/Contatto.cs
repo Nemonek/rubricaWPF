7fa7b6e32ca54fb536f6e280117d977b9fd304b7
Riccardo Piredda <127589992+Nemonek@users.noreply.github.com>
2023-10-24 22:24:40 +0200

    Lezione 18/03/2023

--- Piredda.Riccardo.4i.rubricaWPF/Contatto.cs
+++ Piredda.Riccardo.4i.rubricaWPF/Contatto.cs
@@ -11,19 +11,114 @@ namespace Piredda.Riccardo._4i.rubricaWPF
         // The microsoft convention for the private attributes of a class is:
         // for privates member use the underscore before the name;
         // for the property use the first letter lower case.
-        private string _nome;
+        //private string _nome;
         private string _cognome;
-        private string _numero;
+        private string _telefono;
+        private string _Email;
+        private int _numero;
 
-        //public Contatto(string nom, string cog, string num)
-        //{
-        //    Nome = nom;
-        //    Cognome = cog;
-        //    Numero = num;
-        //}
+        public Contatto() { }
 
-        public string Nome { get => _nome; set => _nome = value; }
-        public string Cognome { get => _cognome; set => _cognome = value; }
-        public string Numero { get => _numero; set => _numero = value; }
+        // Il costruttore è meglio che passi anche lui dal getter e dal setter, per evitare che assegni direttamente un valore non consono.
+        public Contatto(string nom, string cog, int num, string e, string Tel)
+        {
+            Nome = nom;
+            Cognome = cog;
+            Numero = num;
+            Email = e;
+            Telefono = Tel;
+        }
+
+        public Contatto(string r)
+        {
+            string[] values = r.Split(';');
+            this.Nome = values[0];
+            this.Cognome = values[1];
+            this.Telefono = values[2];
+            this.Email = values[3];
+            this.Numero = int.Parse(values[4]);
+        }
+
+        /*
+         * 04/10/2023
+         * La differenza tra le due property scritte così, a livello di compilazione, non cambia nulla, l'assembly generato è uguale.
+         * Quella di default è la seconda versione.
+         * visibilità tipo nome aperta get set chiusa => struttura della property.
+         * Nei get e set è possibile inserire del codice:
+            get {
+                // Corpo del get
+            }
+         * 
+         * Esempio:
+            public string Nome {
+                get { 
+                    return _nome;  
+                } 
+                set { 
+                    _nome = value;
+                } 
+            }
+         * 
+         * Possiamo operare nel get e nel set come se fosse una funzione.
+         * Se modificassimo il getter di numero con 
+            public int Numero {
+                get { 
+                    _numero++;          // Qua avremmo una modifica effettiva del campo privato, che passerebbe da 0 a 1, salvo modifiche fatte dal main.
+                    return _numero + 1; // Qua non viene modificato il campo privato.
+                } 
+                set { 
+                    _numero = value;
+                } 
+            }
+         * 
+         * Alla chiamata del setter, quindi facendo per esempio Numero = 3;
+         * il valore assegnato alla property, quindi in sto caso 3, sarà considerato parametro implicito del setter, e prenderà il nome di 'value'.
+            set { 
+                 _numero = value;
+            } 
+         * 
+         * Se scriviamo c.Numero++;
+         * Il setter è costretto a richiamare il getter, che ritorna al setter il valore del campo, poi il setter incrementa il valore passato con il ++ AL DI FUORI DELLA CLASSE (modifica il parametro value)
+         * poi lo inserisce nel campo privato.
+         * 
+         * Per evitare che l'utente isnerisca un valore che non rispetta i parametri di validità, siccome il get ed il set possono avere un corpo, come un metodo/funzione, si 
+         * può inserire un controllo, che nel caso di una tentata assegnazione di un valroe non consono, sollevi un eccezione
+         * 
+         * Un altro modo per scrivere la property è:
+            public Nome { set; get; }
+         * In questo caso non c'è bisogno di esplicitare l'esistenza del campo privato, poichè verrà creata automaticamente; NON sarà accessibile e/o visualizzabile
+         * come se fosse un campo privato.
+         * 
+         * NOTA BENE: si possono creeare property di sola lattura così:
+            public Nome { private set; get; }
+         *
+         *
+         *
+         *
+         *
+         *
+
+        */
+
+        public string Nome { set; get; }
+        public string Cognome { 
+            get => _cognome; 
+            set => _cognome = value; 
+        }
+        public int Numero { 
+            get
+            {
+                return _numero;
+            } 
+            set
+            {
+                if ( value < 0 || value > 100 )
+                    throw new ArgumentOutOfRangeException();
+                _numero = value;
+            } 
+        }
+
+        public string Email { get => _Email; set => _Email = value; }
+        public string Telefono { get => _telefono; set => _telefono = value; }
     }
 }
--- Piredda.Riccardo.4i.rubricaWPF/MainWindow.xaml.cs
+++ Piredda.Riccardo.4i.rubricaWPF/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Piredda.Riccardo._4i.rubricaWPF
 {
@@ -23,16 +12,106 @@ namespace Piredda.Riccardo._4i.rubricaWPF
         public MainWindow()
         {
             InitializeComponent();
-            Contatto c = new();
-            c.Nome = string.Empty;
-            c.Cognome = string.Empty;
-            c.Numero = string.Empty;
+            // Tutta la roba va inserita all'interno dell'evento Loaded della MainWindow.
+        }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            // Non avendo 100 elementi inizializzati, ma messi a null, il binding cercava di visualizzzre oggetti null, e sollevava un exception; mettento 3, ed inizializzando 3, non ci sono problemi
+            // Se volessimo ovviare a questo prpblemi basterebbe riempire tutta la lunghezza dell'array di contatti vuoti, senza nome cognome ecc..
             Contatto[] Contatti = new Contatto[100];
-            Contatti[0] = c;
+            for (int i = 0; i < Contatti.Length; i++)
+            {
+                Contatti[i] = new();
+            }
 
-            Contatti[0].Nome = "Riccardo";
-            Contatti[0].Cognome = "Piredda";
+
+            // Siccome l'apertura di un file può causare un eccezione per diversi motivi
+            try
+            {
+                // Per farlo copiare sempre nella cartella in cui il programma va a cercare si aprono le proprietà del file: tasto destro -> proprietà, poi su copia si mette 'copia sempre'
+                StreamReader sr = new("Dati.csv");
+                sr.ReadLine();
+                string riga = string.Empty; // equivalent of ""
+
+                // Per leggere tutte le righe singolarmente possiamo ciclare con un while ogni riga una per una usando l'attributo endofstream, che definisce se siamo alla fine della stream o meno:
+                // va negato perchè di default, se non siamo già alla fine della stream, il valore sarà false, e per ciclare deve essere true.
+                // Quando poi arriveremmo alla fine endofstream passerà a true interrompendo il ciclo while.
+
+                // Nel caso in cui abbiamo 3 righe vuote, poi la 4 ha qualcosa, le righe vuote prima di quella piena saranno considerate righe, tutto quello sotto, se sono righe vuote verrà ignorato.7
+                int i = 0;
+                while ( !sr.EndOfStream )
+                {
+                    riga = sr.ReadLine();
+
+                    Contatti[i] = new(riga);
+                    i++;
+                }
+
+                //riga = sr.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+            //// Con la lista viene visuallizata nella datagrid una riga in più poichè consente di aggiungere manualmente elementi senza implementare il codice, ma è già implementato.         
+            ////List<Contatto> Contatti = new();
+            //Contatto c = new();
+
+            //// Chiamate ai setter
+            //c.Nome = "Riccardo";
+            //c.Cognome = "Piredda";
+            //c.Numero = 1;
+            //c.Email = "Riccardo@gmail.com";
+            //c.Telefono = "3516856899";
+
+            //try
+            //{
+            //    c.Numero = 101;
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+            //Contatti[0] = c;
+            ////Contatti.Add(c);
+
+            //// Costruttore implicito: non è neccessario avere un costruttore che supporta parametri per fare questo
+            //Contatto c1 = new()
+            //{
+            //    Nome = "Riccardo",
+            //    Cognome = "Piredda",
+            //    Numero = 2,
+            //    Email = "Riccardo@gmail.com",
+            //    Telefono = "356856897"
+            //};
+
+            //Contatti[1] = c1;
+            ////Contatti.Add(c1);
+
+            //// Ulteriore uso della target typed new expression.
+            //Contatti[2] = new()
+            //{
+            //    Nome = "Roberto",
+            //    Cognome = "Bianchi",
+            //    Numero = 3,
+            //    Email = "Riccardo@gmail.com",
+            //    Telefono = "3515879970222"
+            //};
+
+            ////Contatti.Add( new() {
+            ////    Nome = "Roberto",
+            ////    Cognome = "Bianchi",
+            ////    Numero = 3
+            ////});
+
+            gdDati.ItemsSource = Contatti;
+
+            // In C# il valore di default alla creazione di un intero è 0, che viene assegnato automaticamente.
+            int valore;
         }
     }
 }
