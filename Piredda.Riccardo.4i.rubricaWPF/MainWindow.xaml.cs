b1059fe54b94231e7b7bacdaf5469a04b5431703
Riccardo Piredda <127589992+Nemonek@users.noreply.github.com>
2023-11-15 08:18:55 +0100

    Lezione 8-11-2023

--- Piredda.Riccardo.4i.rubricaWPF/MainWindow.xaml.cs
+++ Piredda.Riccardo.4i.rubricaWPF/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Windows;
+using System.Windows.Media;
 
 namespace Piredda.Riccardo._4i.rubricaWPF
 {
@@ -18,40 +19,54 @@ namespace Piredda.Riccardo._4i.rubricaWPF
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
-            // Non avendo 100 elementi inizializzati, ma messi a null, il binding cercava di visualizzzre oggetti null, e sollevava un exception; mettento 3, ed inizializzando 3, non ci sono problemi
-            // Se volessimo ovviare a questo prpblemi basterebbe riempire tutta la lunghezza dell'array di contatti vuoti, senza nome cognome ecc..
-            Contatto[] Contatti = new Contatto[100];
-            for (int i = 0; i < Contatti.Length; i++)
-            {
-                Contatti[i] = new();
-            }
 
 
+            
             // Siccome l'apertura di un file può causare un eccezione per diversi motivi
             try
             {
+
+                const int MAX = 100;
+
+                Contatto[] Contatti = new Contatto[MAX];
+
                 // Per farlo copiare sempre nella cartella in cui il programma va a cercare si aprono le proprietà del file: tasto destro -> proprietà, poi su copia si mette 'copia sempre'
                 StreamReader sr = new("Dati.csv");
                 sr.ReadLine();
-                string riga = string.Empty; // equivalent of ""
+                string riga = string.Empty; // equivalente di ""
+
+                // Non avendo 100 elementi inizializzati, ma messi a null, il binding cercava di visualizzzre oggetti null, e sollevava un exception; mettento 3, ed inizializzando 3, non ci sono problemi
+                // Se volessimo ovviare a questo prpblemi basterebbe riempire tutta la lunghezza dell'array di contatti vuoti, senza nome cognome ecc..
+                
+
+                int idx = 0;
 
                 // Per leggere tutte le righe singolarmente possiamo ciclare con un while ogni riga una per una usando l'attributo endofstream, che definisce se siamo alla fine della stream o meno:
                 // va negato perchè di default, se non siamo già alla fine della stream, il valore sarà false, e per ciclare deve essere true.
                 // Quando poi arriveremmo alla fine endofstream passerà a true interrompendo il ciclo while.
 
                 // Nel caso in cui abbiamo 3 righe vuote, poi la 4 ha qualcosa, le righe vuote prima di quella piena saranno considerate righe, tutto quello sotto, se sono righe vuote verrà ignorato.7
-                int i = 0;
-                while ( !sr.EndOfStream )
+                while (!sr.EndOfStream && idx < MAX)
                 {
                     riga = sr.ReadLine();
 
-                    Contatti[i] = new(riga);
-                    i++;
+                    Contatti[idx++] = new(riga);
                 }
+
+                while(idx < MAX)
+                {
+                    Contatti[idx++] = new();
+                }
+
+                idx = 0;
+
+                gdDati.ItemsSource = Contatti;
+
+
 
                 //riga = sr.ReadLine();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
@@ -108,10 +123,20 @@ namespace Piredda.Riccardo._4i.rubricaWPF
             ////    Numero = 3
             ////});
 
-            gdDati.ItemsSource = Contatti;
 
             // In C# il valore di default alla creazione di un intero è 0, che viene assegnato automaticamente.
             int valore;
+        }
+
+        private void gdDati_LoadingRow(object sender, System.Windows.Controls.DataGridRowEventArgs e)
+        {
+            Contatto prova = e.Row.DataContext as Contatto;
+
+            if(prova != null && prova.Nome != null)
+            {
+                string tmp = prova.Nome.Trim().ToLower();
+                e.Row.Background = (tmp[0] == 'm') ? Brushes.Yellow : Brushes.Transparent;
+            } 
         }
     }
 }
